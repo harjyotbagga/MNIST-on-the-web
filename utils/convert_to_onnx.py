240c032c45979d5ed72e14263ef29700d36df469
harjyotbagga <imharjyotbagga@gmail.com>
2021-07-03 02:28:09 +0530

    updating utils

--- utils/convert_to_onnx.py
+++ utils/convert_to_onnx.py
@@ -1,14 +1,14 @@
 import torch
 
-from ..model.model import Net
+from model import Net
 
 
 def main():
   pytorch_model = Net()
-  pytorch_model.load_state_dict(torch.load('../model/MNIST_model.pt'))
+  pytorch_model.load_state_dict(torch.load('MNIST_model.pt'))
   pytorch_model.eval()
   dummy_input = torch.zeros(280 * 280 * 4)
-  torch.onnx.export(pytorch_model, dummy_input, '../model/onnx_model.onnx', verbose=True)
+  torch.onnx.export(pytorch_model, dummy_input, 'onnx_model.onnx', verbose=True)
 
 
 if __name__ == '__main__':

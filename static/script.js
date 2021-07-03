7f84fcd695d3bd68246ec6000fcf1cc9f5c58577
harjyotbagga <imharjyotbagga@gmail.com>
2021-07-04 01:00:36 +0530

    removing unwanted comments

--- static/script.js
+++ static/script.js
@@ -11,11 +11,9 @@ let lastX = 0;
 let lastY = 0;
 let autoClear = false;
 
-// Load our model.
 const sess = new onnx.InferenceSession();
 const loadingModelPromise = sess.loadModel("./onnx_model.onnx");
 
-// Add 'Draw a number here!' to the canvas.
 ctx.lineWidth = 28;
 ctx.lineJoin = "round";
 ctx.font = "28px sans-serif";
@@ -24,7 +22,6 @@ ctx.textBaseline = "middle";
 ctx.fillStyle = "#212121";
 ctx.fillText("Loading...", CANVAS_SIZE / 2, CANVAS_SIZE / 2);
 
-// Set the line color for the canvas.
 ctx.strokeStyle = "#212121";
 
 function clearCanvas() {
@@ -37,7 +34,6 @@ function clearCanvas() {
 }
 
 function drawLine(fromX, fromY, toX, toY) {
-  // Draws a line from (fromX, fromY) to (toX, toY).
   ctx.beginPath();
   ctx.moveTo(fromX, fromY);
   ctx.lineTo(toX, toY);
@@ -47,7 +43,6 @@ function drawLine(fromX, fromY, toX, toY) {
 }
 
 async function updatePredictions() {
-  // Get the predictions for the canvas data.
   const imgData = ctx.getImageData(0, 0, CANVAS_SIZE, CANVAS_SIZE);
   const input = new onnx.Tensor(new Float32Array(imgData.data), "float32");
 
@@ -75,12 +70,6 @@ function canvasMouseDown(event) {
   const x = event.offsetX / CANVAS_SCALE;
   const y = event.offsetY / CANVAS_SCALE;
 
-  // To draw a dot on the mouse down event, we set laxtX and lastY to be
-  // slightly offset from x and y, and then we call `canvasMouseMove(event)`,
-  // which draws a line from (laxtX, lastY) to (x, y) that shows up as a
-  // dot because the difference between those points is so small. However,
-  // if the points were the same, nothing would be drawn, which is why the
-  // 0.001 offset is added.
   lastX = x + 0.001;
   lastY = y + 0.001;
   canvasMouseMove(event);
@@ -104,11 +93,6 @@ function bodyMouseUp() {
 }
 
 function bodyMouseOut(event) {
-  // We won't be able to detect a MouseUp event if the mouse has moved
-  // ouside the window, so when the mouse leaves the window, we set
-  // `isMouseDown` to false automatically. This prevents lines from
-  // continuing to be drawn when the mouse returns to the canvas after
-  // having been released outside the window.
   if (!event.relatedTarget || event.relatedTarget.nodeName === "HTML") {
     isMouseDown = false;
   }
